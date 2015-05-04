478b5e376197a1afe50b44ae705324643dfca8cb
slewarne <slewarne@serena.com>
2015-05-04 09:21:02 -0700

    Updates to fix build

---
+++ Integrations/Setup.aspx
@@ -0,0 +1,77 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="Setup.aspx.vb" Inherits="Serena_Integrations.Setup" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 34px;
+        }
+        </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Panel ID="panelSecret" runat="server" Visible="False">
+            <table>
+                <tr><td>
+                    <asp:Label ID="Label1" runat="server" Text="Please paste the contents of the clients_secret file below:"></asp:Label>
+                    </td><td class="auto-style1"></td></tr>
+                <tr><td>
+                    <asp:TextBox ID="txtSecrets" runat="server" Height="144px" Rows="20" Width="616px" TextMode="MultiLine"></asp:TextBox>
+                    </td><td class="auto-style1"></td></tr>
+                <tr><td>
+                    <asp:Button ID="BtnSaveSecrets" runat="server" Text="Save Clients_Secrets" />
+                    </td><td class="auto-style1">
+                        <asp:HiddenField ID="cmdAction" runat="server" Visible="False" />
+                    </td></tr>
+            </table>
+        </asp:Panel>
+        <asp:Panel ID="panelRequestToken" runat="server" Visible="False">
+            <table>
+                <tr><td>
+                    <asp:Label ID="Label5" runat="server" Text="Clients_Secret info has been found.  Follow instructions below to request an access token."></asp:Label>
+                    </td><td class="auto-style1"></td></tr>
+                <tr><td>
+                    <asp:Label ID="Label2" runat="server" Text="1. Click "></asp:Label>
+                    <asp:HyperLink ID="lnkV1" runat="server" Target="_blank">here </asp:HyperLink>
+                    <asp:Label ID="Label4" runat="server" Text="to request a VersionOne access token.  You will be prompted to login and confirm API access.  "></asp:Label>
+                    </td><td class="auto-style1"></td></tr>
+                <tr><td>
+                    <asp:Label ID="Label7" runat="server" Text="2. Once you login to V1 and confirm access, a token will appear."></asp:Label>
+                    &nbsp; You have 60 seconds to complete step 3.</td><td class="auto-style1"></td></tr>
+                <tr><td>
+                    <asp:Label ID="Label9" runat="server" Text="3. Paste the V1 token below and click &quot;Save Request Token&quot; to continue."></asp:Label>
+                    </td><td class="auto-style1"></td></tr>
+                <tr><td>
+                    <asp:Label ID="lblTokenError" runat="server" Text="Please paste the V1 token below to continue." Visible="False"></asp:Label>
+                    </td><td class="auto-style1"></td></tr>
+
+                <tr><td>
+                    <asp:TextBox ID="txtRequestToken" runat="server" Height="144px" Rows="20" Width="616px" TextMode="MultiLine"></asp:TextBox>
+                    </td><td class="auto-style1"></td></tr>
+                <tr><td>
+                    <asp:Button ID="Button2" runat="server" Text="Save Request Token" />
+                    </td><td class="auto-style1">
+                        &nbsp;</td></tr>
+            </table>
+        </asp:Panel>
+        <asp:Panel ID="pnlReady" runat="server" Visible="False">
+            <table>
+                <tr><td>
+                    <asp:Label ID="Label3" runat="server" Text="Secrets and Credentials Files found, continue to test page."></asp:Label>
+                    </td><td class="auto-style1"></td></tr>
+                <tr><td>
+                    <asp:Button ID="btnContinue" runat="server" Text="Continue" />
+                    </td><td class="auto-style1">
+                        &nbsp;</td></tr>
+            </table>
+        </asp:Panel>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Integrations/Setup.aspx.vb
@@ -0,0 +1,109 @@
+﻿Imports System.IO
+Imports System.Net
+Imports OAuth2Client
+'
+
+Public Class Setup
+    Inherits System.Web.UI.Page
+
+    Dim myPath As String = Context.Server.MapPath("~/config")
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        Dim myFile As FileStream
+
+
+        If Page.IsPostBack = False Then
+
+            'check to see if we have the two files we need.  First is the secrets file.
+            If File.Exists(myPath & "\client_secrets.json") And File.Exists(myPath & "\stored_credentials.json") Then
+                pnlReady.Visible = True
+            Else
+                If Not File.Exists(myPath & "\client_secrets.json") Then
+                    'create the directory if it doesn't exist
+                    If Not Directory.Exists(myPath) Then
+                        Directory.CreateDirectory(myPath)
+                    End If
+                    'we have no client secrets file, prompt the user for it.
+                    panelSecret.Visible = True
+                    cmdAction.Value = "save_secrets"
+                Else
+                    'prep the request URL
+                    showRequestPanel()
+                End If
+            End If
+           
+        Else
+            'postback
+            If cmdAction.Value = "save_secrets" Then
+
+                If txtSecrets.Text = "" Then
+                    panelSecret.Visible = True
+                    cmdAction.Value = "save_secrets"
+                Else
+                    'store the secrets file
+                    myFile = File.OpenWrite(myPath & "\client_secrets.json")
+                    Dim secrets As Byte() = New UTF8Encoding(True).GetBytes(txtSecrets.Text)
+                    myFile.Write(secrets, 0, secrets.Length)
+                    myFile.Close()
+                    panelSecret.Visible = False
+                    showRequestPanel()
+                End If
+
+            ElseIf cmdAction.Value = "request_cert" Then
+                'validate that a cert was entered
+                If txtRequestToken.Text = "" Then
+                    lblTokenError.Visible = True
+                Else
+                    'attempt to get the token.
+                    Dim client As New webclient
+                    Dim reqparm As New Specialized.NameValueCollection
+                    Dim credentials As IStorage = New Storage.JsonFileStorage(myPath & "\client_secrets.json", myPath & "\client_secrets.json")
+                    Dim url As String = ConfigurationManager.AppSettings("voURL") & "oauth.v1/token"
+                    client.Headers.Add("Content-Type", "application/x-www-form-urlencoded")
+
+                    reqparm.Add("client_id", credentials.GetSecrets.client_id)
+                    reqparm.Add("client_secret", credentials.GetSecrets.client_secret)
+                    reqparm.Add("code", txtRequestToken.Text)
+                    reqparm.Add("grant_type", "authorization_code")
+                    reqparm.Add("redirect_uri", credentials.GetSecrets.redirect_uris(0).ToString)
+
+
+                    Dim responsebytes = client.UploadValues(url, "POST", reqparm)
+                    Dim responsebody = (New Text.UTF8Encoding).GetString(responsebytes)
+                    'store it
+                    myFile = File.OpenWrite(myPath & "\stored_credentials.json")
+                    myFile.Write(New UTF8Encoding(True).GetBytes(responsebody), 0, responsebody.Length)
+                    myFile.Close()
+                    panelRequestToken.Visible = False
+                    pnlReady.Visible = True
+                    cmdAction.Value = "done"
+
+                End If
+               
+            End If
+        End If
+
+
+    End Sub
+
+    Private Sub showRequestPanel()
+        'set the URL for V1
+        Dim credentials As IStorage = New Storage.JsonFileStorage(myPath & "\client_secrets.json", myPath & "\client_secrets.json")
+
+        Dim url As String = ConfigurationManager.AppSettings("voURL") & "oauth.v1/auth?" & _
+                            "response_type=code&client_id=" & credentials.GetSecrets.client_id & "&redirect_uri=" & _
+                            credentials.GetSecrets.redirect_uris(0).ToString & "&scope=apiv1 query-api-1.0"
+
+        lnkV1.NavigateUrl = url
+        'show validation panel
+        panelRequestToken.Visible = True
+        cmdAction.Value = "request_cert"
+    End Sub
+
+    Protected Sub Button3_Click(sender As Object, e As EventArgs) Handles btnContinue.Click
+        Response.Redirect("test.aspx", True)
+    End Sub
+
+   
+End Class
